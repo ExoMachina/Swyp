61ce39fa38a48e5004e5dfedc4660f731dd06db8
Alexander H. List <alist@listprojects.com>
2011-08-01 15:02:44 -0700

    added external libSwyp headers for mandatory classes

---
+++ libSwyp.h
@@ -0,0 +1,13 @@
+/*
+ *  libSwyp.h
+ *  swyp
+ *
+ *  Created by Alexander List on 8/1/11.
+ *	Copyright 2011 ExoMachina. Some rights reserved -- see included 'license' file.
+ *
+ */
+
+#import "swypContentInteractionManager.h"
+#import "swypFileTypeString.h"
+#import "swypConnectionSession.h"
+#import "swypWorkspaceViewController.h"
