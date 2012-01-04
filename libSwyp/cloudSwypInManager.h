3da5be5b1bfb4294cbe6c3e5c73084810f064bd8
Alexander List <alist@listprojects.com>
2012-01-04 17:28:20 -0500

    updated license to express intent of developers better

---
+++ libSwyp/cloudSwypInManager.h
@@ -0,0 +1,13 @@
+//
+//  cloudSwypInManager.h
+//  swyp
+//
+//  Created by Alexander List on 1/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cloudSwypInManager : NSObject
+
+@end
