92711fc69d66e89472b707a8464094b7b7a5c580
Alexander List <alist@listprojects.com>
2012-01-04 17:33:12 -0500

    added new files for cloud swyp

---
+++ libSwyp/cloudSwypNetService.h
@@ -0,0 +1,13 @@
+//
+//  cloudSwypNetService.h
+//  swyp
+//
+//  Created by Alexander List on 1/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cloudSwypNetService : NSObject
+
+@end
---
+++ libSwyp/cloudSwypNetService.m
@@ -0,0 +1,13 @@
+//
+//  cloudSwypNetService.m
+//  swyp
+//
+//  Created by Alexander List on 1/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import "cloudSwypNetService.h"
+
+@implementation cloudSwypNetService
+
+@end
