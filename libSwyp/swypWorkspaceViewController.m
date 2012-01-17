118748adb166c70bba0f62baba8f262740054e09
Ethan Sherbondy <sherbondye@gmail.com>
2012-01-17 01:23:05 -0500

    fixed arrows for ios4.

--- libSwyp/swypWorkspaceViewController.m
+++ libSwyp/swypWorkspaceViewController.m
@@ -227,7 +227,12 @@
 
 -(void) viewWillAppear:(BOOL)animated{
 	[super viewWillAppear:animated];
-    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
+    
+    if ([[[UIDevice currentDevice] systemVersion] intValue] >= 5) {
+        [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
+    } else {
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
+    }
 }
 
 -(void) viewWillDisappear:(BOOL)animated{
@@ -253,10 +258,14 @@
     curlButton.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"top_curl"]];
     [curlButton.layer setOpaque:NO];
     
+    // weird ios4 bug
+    if ([[[UIDevice currentDevice] systemVersion] intValue] >= 5) {
+        [curlButton addTarget:self action:@selector(animateArrows:) forControlEvents:UIControlEventTouchDown];
+    }
+    
     [curlButton addTarget:self action:@selector(leaveWorkspaceButtonPressed:) 
          forControlEvents:UIControlEventTouchUpInside];
-    
-    [curlButton addTarget:self action:@selector(animateArrows:) forControlEvents:UIControlEventTouchDown];
+         
     [curlButton addTarget:self action:@selector(stopArrows:) forControlEvents:(UIControlEventTouchCancel|UIControlEventTouchUpInside|UIControlEventTouchDragOutside)];
     
     UISwipeGestureRecognizer *swipeDownRecognizer = [[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(leaveWorkspaceButtonPressed:)] autorelease];
