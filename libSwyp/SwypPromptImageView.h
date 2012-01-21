fb6a0b12887b91801864837b0dc250cf9e4b7585
Ethan Sherbondy <sherbondye@gmail.com>
2012-01-21 15:06:05 -0500

    documenting swypThumbView.

--- libSwyp/SwypPromptImageView.h
+++ libSwyp/SwypPromptImageView.h
@@ -8,13 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+/** This is the view for the swyp icon in the center of the swyp Workspace
+ **/
+
 @interface SwypPromptImageView : UIView {
     UIImageView *_imageView;
     UIView *_bluetoothView;
 }
 
-///This performs a flip animation to/from the regular swyp icon once
-///bluetooth is on and ready to accept connections.
+/** This performs a flip animation to/from the regular swyp icon once
+    bluetooth is on and ready to accept connections.
+ **/
 - (void) setBluetoothReady:(BOOL)isReady;
 
 @end
