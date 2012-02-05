26bba89d366c2d806c3287f84b91eb57c74f33f6
Alexander List <alist@listprojects.com>
2012-02-04 19:02:54 -0500

    swyp workspace embedded views are now full featured

--- libSwyp/swypWorkspaceView.h
+++ libSwyp/swypWorkspaceView.h
@@ -13,12 +13,21 @@
 #import "swypWorkspaceBackgroundView.h"
 #import "swypPromptImageView.h"
 
+
+/** 
+ This UIView subclass is used by the swypWorkspaceViewController for displaying its main view, and for generating workspace views that can be embedded in custom application code.
+ 
+ This class is instantiated using workspaceViewForEmbeddedSwypIn. 
+ */
 @interface swypWorkspaceView : UIView
-@property (nonatomic, retain) swypWorkspaceBackgroundView * backgroundView;
-@property (nonatomic, retain) 	UIImageView	*						prettyOverlay;
-@property (nonatomic, retain) SwypPromptImageView *				swypPromptImageView;
-@property (nonatomic, retain) UIButton *							swypNetworkInterfaceClassButton;
+@property (nonatomic, retain) swypWorkspaceBackgroundView *	backgroundView;
+@property (nonatomic, retain) UIImageView	*				prettyOverlay;
+@property (nonatomic, retain) SwypPromptImageView *			swypPromptImageView;
+@property (nonatomic, retain) UIButton *					swypNetworkInterfaceClassButton;
 
 
+/**This initialization is handled by swypWorkspaceViewController in workspaceViewForEmbeddedSwypIn
+ @param workspace is the viewController responsible for managing all the events associated with the buttons and gesture recognizers.
+ */
 -(swypWorkspaceView*) initWithFrame:(CGRect)frame workspaceTarget:(id)workspace;
 @end
